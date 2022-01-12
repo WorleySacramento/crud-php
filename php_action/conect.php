c64a056dee783f4f866342c826f3237257fe20df
Worley Sacramento <78088577+WorleySacramento@users.noreply.github.com>
2022-01-11 23:36:34 -0300

    up

---
+++ php_action/conect.php
@@ -0,0 +1,16 @@
+<?php
+$server = "localhost";
+$user = "root";
+$password = "";
+$bd = "form_db";
+
+if (mysqli_connect( $server, $user, $password, $bd)){
+    //echo "Conectado com sucesso!!";
+}else
+echo "ERRO!!";
+
+
+
+
+
+?>
